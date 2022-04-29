91808dc92b0cdb97d69672cb7cc4a34b3be4a92b
Antonio Carlos <goodcorretora@gmail.com>
2022-04-29 15:18:04 -0300

    Restricao de conteudo: cliente so recupera seus pedidos

--- cursomc/src/main/java/comspring/cursomc/repositories/PedidoRepository.java
+++ cursomc/src/main/java/comspring/cursomc/repositories/PedidoRepository.java
@@ -1,11 +1,18 @@
 package comspring.cursomc.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import comspring.cursomc.domain.Cliente;
 import comspring.cursomc.domain.Pedido;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
+	
+	@Transactional(readOnly=true)
+	Page<Pedido> findByCliente(Cliente cliente, Pageable pageRequest);
 
 }
--- cursomc/src/main/java/comspring/cursomc/services/PedidoService.java
+++ cursomc/src/main/java/comspring/cursomc/services/PedidoService.java
@@ -4,9 +4,12 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
+import comspring.cursomc.domain.Cliente;
 import comspring.cursomc.domain.ItemPedido;
 import comspring.cursomc.domain.PagamentoComBoleto;
 import comspring.cursomc.domain.Pedido;
@@ -14,6 +17,8 @@ import comspring.cursomc.domain.enums.EstadoPagamento;
 import comspring.cursomc.repositories.ItemPedidoRepository;
 import comspring.cursomc.repositories.PagamentoRepository;
 import comspring.cursomc.repositories.PedidoRepository;
+import comspring.cursomc.security.UserSS;
+import comspring.cursomc.services.exceptions.AuthorizationException;
 import comspring.cursomc.services.exceptions.ObjectNotFoundException;
 
 
@@ -25,13 +30,13 @@ public class PedidoService {
 	
 	@Autowired
 	private BoletoService boletoService;
-
+	
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
-
+	
 	@Autowired
 	private ItemPedidoRepository itemPedidoRepository;
-
+	
 	@Autowired
 	private ProdutoService produtoService;
 	
@@ -41,16 +46,12 @@ public class PedidoService {
 	@Autowired
 	private EmailService emailService;
 	
-
-	
-	
 	public Pedido find(Integer id) {
 		Optional<Pedido> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
 	}
 	
-	@Transactional
 	public Pedido insert(Pedido obj) {
 		obj.setId(null);
 		obj.setInstante(new Date());
@@ -72,5 +73,15 @@ public class PedidoService {
 		itemPedidoRepository.saveAll(obj.getItens());
 		emailService.sendOrderConfirmationEmail(obj);
 		return obj;
+	}
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSS user = UserService.authenticated();
+		if (user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente =  clienteService.find(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
 	}
 }
